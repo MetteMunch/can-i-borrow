88270bca3cda5921aa51f159b1adc8fc3cd6cfc7
MetteMunch <meha0005@stud.kea.dk>
2025-12-13 11:36:02 +0100

    Nye backend endpoints er tjekket op imod Postman

--- backend/routes/itemsRouter.js
+++ backend/routes/itemsRouter.js
@@ -10,13 +10,6 @@ router.get("/", async (req, res) => {
     res.send({ data: allItems });
 });
 
-router.get("/:itemId", async (req, res) => {
-    const specificItem = await db.get("SELECT items.*, users.fullname AS owner_name, users.phone AS owner_phone FROM items JOIN users ON users.id = items.owner_id WHERE items.id = ?", req.params.itemId);
-    res.status(200).send({
-        data: specificItem
-    });
-});
-
 router.get("/my-items", isLoggedIn, async (req, res) => {
     const allOfMyItems = await db.all(`SELECT * FROM items WHERE owner_id = ?`, req.session.user.id);
     console.log("Prøver at hente alle mine items", allOfMyItems)
@@ -24,6 +17,15 @@ router.get("/my-items", isLoggedIn, async (req, res) => {
         data: allOfMyItems
     });
 });
+
+
+router.get("/:itemId", async (req, res) => {
+    const specificItem = await db.get("SELECT items.*, users.fullname AS owner_name, users.phone AS owner_phone FROM items JOIN users ON users.id = items.owner_id WHERE items.id = ?", req.params.itemId);
+    res.status(200).send({
+        data: specificItem
+    });
+});
+
 
 router.post("/", isLoggedIn, async (req, res) => {
     const {item, description, image_url} = req.body;
