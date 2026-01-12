3f258c241d471cb99f75d05978a2349d38d60cdc
MetteMunch <meha0005@stud.kea.dk>
2026-01-12 09:00:18 +0100

    comments og linting

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -2,5 +2,5 @@ import { io } from 'socket.io-client';
 
 export const socket = io('http://localhost:8080', {
   withCredentials: true, //clienten sender cookie med socket-forbindelsen, så serveren kan se hvilken bruger der er på
-  autoConnect: false,//bruger bliver ikke automatisk forbundet..men vi styrer det (det sker ved login)
+  autoConnect: false, //bruger bliver ikke automatisk forbundet..men vi styrer det (det sker ved login)
 });
